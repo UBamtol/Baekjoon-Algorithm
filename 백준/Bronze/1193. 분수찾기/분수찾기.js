9cc7b5f39ff4a32d7407ca2537a3596851a2cb8a
UBamtol <98325285+UBamtol@users.noreply.github.com>
2022-05-31 00:49:22 +0900

    [Bronze I] Title: 분수찾기, Time: 144 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/1193. 분수찾기/분수찾기.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const inputData = fs.readFileSync(0, "utf8").toString().trim();
+
+let A = inputData;
+let answer = 0;
+let count = 0;
+let num = 0;
+let numerator = 0; // 분자
+let denominator = 0; // 분모
+
+while(true) {
+  count++;
+  if(A > num+count) {
+    num += count;
+  } else {
+    if(count % 2 === 0) {
+      numerator = A-num;
+      denominator = (count+1)-numerator;
+      break;
+    } else {
+      denominator = A-num;
+      numerator = (count+1)-denominator;
+      break;
+    }
+  }
+
+}
+console.log(`${numerator}/${denominator}`);
