687976bdd7ab4f089f3ef45a9dabda66602177a9
UBamtol <98325285+UBamtol@users.noreply.github.com>
2025-03-08 19:57:59 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.45 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -0,0 +1,23 @@
+function solution(n, words) {
+    var answer = [];
+    let array = [];
+    let mans=0;
+    
+    for(let i = 0; i < words.length;i++){
+        if(mans < n){
+           mans += 1; 
+        } else {
+            mans = 1;
+        }
+        array.push([mans,words[i]])
+    }
+    for(let i = 0; i < words.length-1; i++){
+        if(words.slice(0,i+1).includes(array[i+1][1]) || (words[i][words[i].length-1] !== words[i+1][0])){
+            return answer = [array[i+1][0], Math.ceil((i+2)/n)]
+        } else if(i+1 === array.length-1 && array[i][1] !== array[i+1][1]){
+            return answer = [0,0]
+        } 
+    }    
+
+    return answer;
+}
